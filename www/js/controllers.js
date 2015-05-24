339388b560917e44b1ffb2e4a1a2da55f6feb09a
Ginés Ortiz Sáez <gines.o.saez@gmail.com>
2015-05-24 17:32:26 +0200

    Gps added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,37 +1,48 @@
 angular.module('app.controllers', [])
 
-.controller('MapCtrl', function($scope) {})
+    .controller('MapCtrl', function ($scope) {
+    })
 
-.controller('TrendingCtrl', function($scope, $ionicModal, Happinesses) {
-  $scope.happinesses = [];
-  $scope.happinessRange = [1, 2, 3, 4, 5];
-  $scope.happinessRangeMin = $scope.happinessRange[0];
-  $scope.happinessRangeMax = $scope.happinessRange[$scope.happinessRange.length - 1];
+    .controller('TrendingCtrl', function ($scope, $ionicModal, Happinesses, gpsSV) {
+        $scope.happinesses = [];
+        $scope.happinessRange = [1, 2, 3, 4, 5];
+        $scope.happinessRangeMin = $scope.happinessRange[0];
+        $scope.happinessRangeMax = $scope.happinessRange[$scope.happinessRange.length - 1];
 
-  var happinesses = Happinesses.query(function() {
-    $scope.happinesses = happinesses;
-  });
-  
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/addHappinessModal.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        var happinesses = Happinesses.query(function () {
+            $scope.happinesses = happinesses;
+        });
 
-  // Triggered in the modal to close it
-  $scope.closeAddHappiness = function() {
-    $scope.modal.hide();
-  };
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/addHappinessModal.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Open the modal
-  $scope.openAddHappiness = function() {
-    $scope.modal.show();
-  };
-})
+        // Triggered in the modal to close it
+        $scope.closeAddHappiness = function () {
+            $scope.modal.hide();
+        };
 
-.controller('AboutCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        // Open the modal
+        $scope.openAddHappiness = function () {
+            $scope.modal.show();
+            gpsSV.getCurrentPosition(
+                function onSuccess(location) {
+                    console.log(location.coords.latitude, location.coords.longitude);
+                    console.log('https://www.google.es/maps/place/' + location.coords.latitude + ',' + location.coords.longitude);
+                },
+                function onError(error) {
+                    alert('Error, timeout superado');
+                }
+            );
+
+        };
+    })
+
+    .controller('AboutCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,15 @@
 angular.module('app.services', [])
 
-.factory('Happinesses', function($resource) {
-  return $resource('http://hackhappiness.herokuapp.com/happinesses/:id', { happinessId:'@id' });
-});
+    .factory('Happinesses', function ($resource) {
+        return $resource('http://hackhappiness.herokuapp.com/happinesses/:id', {happinessId: '@id'});
+    })
+
+    .service('gpsSV', function () {
+        this.getCurrentPosition = function (onSuccess, onError) {
+            var options = {
+                timeout: 15000, //miliseconds
+                maximumAge: 180000 //allows the device to answer immediately with a cached position (until 3 minutes since last value)
+            };
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+        };
+    });
